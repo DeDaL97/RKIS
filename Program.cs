ece36a21a16dee348bf0dd470850bf39868613a1
C-conscious <167548148+DeDaL97@users.noreply.github.com>
2024-10-27 00:39:36 +0300

    Lesson 1: A

--- Program.cs
+++ Program.cs
@@ -1,17 +1,10 @@
-﻿namespace B
+﻿namespace A
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            double num1 = +5.5e-2;
-            float num2 = 7.8f;
-            int num3 = 0;
-            long num4 = 2000000000000L;
-            Console.WriteLine(num1);
-            Console.WriteLine(num2);
-            Console.WriteLine(num3);
-            Console.WriteLine(num4);
+            Console.WriteLine(The first step!);
         }
     }
 }
